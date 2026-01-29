e9cde76cd60b5beb53593a40b4fcd172eea03b14
3cgbdg <bogdantytysh5@gmail.com>
2026-01-29 11:34:59 +0200

    feat: updated tech stack - front providers + back typeorm

---
+++ backend/src/strategies/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { Request } from "express";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private readonly configService: ConfigService, @InjectRepository(User) private readonly userRepository: Repository<User>) {
+
+        super({
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                (req: Request) => req.cookies?.['access_token'],
+            ]),
+            ignoreExpiration: false,
+            secretOrKey: configService.get<string>('JWT_SECRET')!,
+        });
+    }
+
+    async validate(payload: any) {
+        const user = await this.prisma.user.findUnique({ where: { id: payload.userId }, select: { id: true } });
+        if (!user)
+            throw new NotFoundException();
+        return user;
+    }
+}
---
+++ frontend/app/providers.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { ReactNode } from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "../store/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+
+const queryClient = new QueryClient();
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
+
+type AppProvidersProps = {
+  children: ReactNode;
+};
+
+export function AppProviders({ children }: AppProvidersProps) {
+  return (
+    <ReduxProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </ReduxProvider>
+  );
+}
+
