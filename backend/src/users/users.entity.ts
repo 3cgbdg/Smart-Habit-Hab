b6bc8db1f843d2a4a268f5abdeb10c7ea66a754b
3cgbdg <bogdantytysh5@gmail.com>
2026-01-29 15:36:08 +0200

    feat: getOwnProfile endpoint

--- backend/src/users/users.entity.ts
+++ backend/src/users/users.entity.ts
@@ -3,7 +3,7 @@ import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateCol
 @Entity({ name: 'users' })
 export class User {
     @PrimaryGeneratedColumn('uuid')
-    id: number;
+    id: string;
     @Column({ unique: true })
     email: string;
     @Column()
